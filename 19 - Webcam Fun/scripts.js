a230af6839239d3ee992ad2f7b10711430d6bb95
Craig Treptow <craig.treptow@gmail.com>
2019-02-25 17:32:08 -0600

    task[19]: Web cams
    This is a whole lot of messing around and trying to debug
    a failing URL.createObjectURL.
    
    Basically, I don't care enough about webcams and browsers to continue
    looking at it.  I'm moving on.

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,37 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  if (navigator.mediaDevices.getUserMedia) {
+    console.log('using getUserMedia');
+    navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+      .then(localMediaStream => {
+        console.log(localMediaStream);
+        video.src = window.URL.createObjectURL(localMediaStream);
+        video.play();
+        })
+      .catch(function (e) { console.log(e.name + ": " + e.message);});
+  } else {
+    console.log('using getWebcam');
+    navigator.getWebcam(
+      { audio: true, video: true }, 
+      localMediaStream => {
+        console.log(localMediaStream);
+        video.src = window.URL.createObjectURL(localMediaStream);
+        video.play();
+      },
+      function () { console.log("Web cam is not accessible."); }
+    );
+  }
+
+  //  navigator.getUserMedia({ video: true, audio: false })
+  //  // navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+  //    .then(localMediaStream => {
+  //      console.log(localMediaStream);
+  //      video.src = window.URL.createObjectURL(localMediaStream);
+  //      // video.play();
+  //    });
+}
+
+getVideo();
